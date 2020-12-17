a410c7b92b701004a1222d557c6bccaba4d3ea4f
Jorge Alfredo Alvarez Guzman <jorgealfredoalvarezguzman@Jorges-MacBook-Pro.local>
2020-12-17 10:47:30 -0500

    Add search functionality to Torre endpoint

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 12:10 - set up Git repo \
 15:00 - set up initial flask app \
 17:00 - test routes \
+
+## Tuesday, December 15, 2020
+10:00 - 15:00  - development
+
+## Wednesday, December 16, 2020
+10:00 - 15:00  - development
+
+## Thursday, December 17, 2020
+9:00 - 11:00 - development
+11:00 - deployment
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import requests
 import json
 
@@ -7,9 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
-    req = requests.get('https://torre.bio/api/bios/1')
-    data = json.loads(req.content)
-    return render_template('index.html', data=data)  
+    return redirect('opportunities')
 
 
 @app.route('/bio/<int:id>', methods = ['GET'])
@@ -19,17 +17,25 @@ def getBio(id):
     return render_template('index.html', data=data) 
 
 
-@app.route('/opportunities', methods = ['GET', 'POST'])
-def getOportunities():
+@app.route('/opportunities/', methods = ['GET', 'POST'])
+@app.route('/opportunities/<string:keyword>', methods = ['GET', 'POST'])
+def getOportunities(keyword=None):
+    req = requests.post('https://search.torre.co/opportunities/_search/?offset=0&size=18&aggregate=false', json = {"test": "test"})
+    data = req.json()
+
+    keyword = request.form.get("search")
+
+    if (keyword):
+        filtered_data = [x for x in data['results'] if (keyword.lower() in json.dumps(x).lower())]
+        return render_template('opportunity.html', opportunities=filtered_data)
+
+    return render_template('opportunity.html', opportunities=data['results'])
+
+
+@app.route('/test', methods = ['GET', 'POST'])
+def test():
     req = requests.post('https://search.torre.co/opportunities/_search/?offset=0&size=10&aggregate=false', json = {"test": "test"})
     data = req.json()
-    return render_template('index.html', data=data) 
-
-
-@app.route('/people', methods = ['GET', 'POST'])
-def getPeople():
-    req = requests.post('https://search.torre.co/people/_search/?[offset=0&size=10&aggregate=false]', json = {"test": "test"})
-    data = req.json()
-    return render_template('index.html', data=data) 
+    return render_template('index.html', data=data['results'])
 
     
